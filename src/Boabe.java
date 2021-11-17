b1aed131883932668b52cb6a1a1bbbef212a6bd6
Gabriel Mihalache <mihalache.gabriel16@gmail.com>
2021-11-17 21:24:26 +0200

    innitial push

---
+++ src/Boabe.java
@@ -0,0 +1,6 @@
+public class Boabe extends AnimalFood{
+    public void bagaBoaba(){
+        System.out.println("baga boabe!");
+    }
+
+}
---
+++ src/WhiteAdopter.java
@@ -0,0 +1,5 @@
+public class WhiteAdopter extends Adopter{
+    public void whitePeople(){
+        System.out.println("Mananc caini.");
+    }
+}
